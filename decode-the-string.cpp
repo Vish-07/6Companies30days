5c71a14d68b436b910bbdebe279e47edbd14bd1d
Vishakha <vvikharankar@students.vnit.ac.in>
2022-01-05 17:02:42 +0530

    Day5-Que3

--- decode-the-string.cpp
+++ decode-the-string.cpp
@@ -29,6 +29,48 @@ class Solution{
         }
         return result;
     }
+
+    string decodeStringUsingStack(string s) 
+    {
+        stack<int> frequency;
+        stack<string> substr;
+        int num = 0;
+        int i = 0;
+        string current = "";
+        while(s[i] != '\0')
+        {
+            if(s[i] == '[')
+            {
+                substr.push(current);
+                frequency.push(num);
+                current = "";
+                num = 0;
+            }
+            else if(s[i] >= '0' && s[i] <= '9')
+            {
+                num = num * 10 + (s[i] - '0');
+            }
+            else if(s[i] == ']')
+            {
+                string temp = current;
+                int freq = frequency.top();
+                frequency.pop();
+                current = substr.top();
+                substr.pop();
+                while(freq--)
+                {
+                    current += temp;
+                }
+                num = 0;
+            }
+            else
+            {
+                current += s[i];
+            }
+            i++;
+        }
+        return current;
+    }
 public:
     string decodedString(string s){
         int i = 0;
