98e1894b1233d83f56fe2509b666362caf9e9152
Vishakha <vvikharankar@students.vnit.ac.in>
2022-01-09 22:03:11 +0530

    Amazon-Que10

---
+++ 25-nuts-and-bolts-problem.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) 
+	{
+	    sort(nuts, nuts + n);
+	    sort(bolts, bolts + n);
+	    
+	}
+
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        char nuts[n], bolts[n];
+        for (int i = 0; i < n; i++) {
+            cin >> nuts[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> bolts[i];
+        }
+        Solution ob;
+        ob.matchPairs(nuts, bolts, n);
+        for (int i = 0; i < n; i++) {
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
