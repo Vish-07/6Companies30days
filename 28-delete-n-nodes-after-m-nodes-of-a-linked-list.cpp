923d2adae141326b1f5baa890f704b9b847dbf70
Vishakha <vvikharankar@students.vnit.ac.in>
2022-01-10 13:51:28 +0530

    Amazon-Que13

---
+++ 28-delete-n-nodes-after-m-nodes-of-a-linked-list.cpp
@@ -0,0 +1,132 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while(node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert(int n1)
+{
+    int n,value;
+    n=n1;
+    struct Node *temp;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp=start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp=temp->next;
+        }
+    }
+}
+
+ // } Driver Code Ends
+/*
+delete n nodes after m nodes
+  The input list will have at least one element  
+  Node is defined as 
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+*/
+class Solution
+{
+    public:
+    void linkdelete(struct Node  *head, int M, int N)
+    {
+        Node* trav = head;
+        while(trav)
+        {
+            int count = M;
+            while(count > 1 && trav)
+            {
+                trav = trav->next;
+                count--;
+            }
+            count = N;
+            Node* curr = trav;
+            while(count && curr)
+            {
+                curr = curr->next;
+                count--;
+            }
+            if(trav && curr)
+            {
+                trav->next = curr->next;
+                trav = trav->next;
+            }
+            else if(trav)
+            {
+                trav->next = NULL;
+                trav = trav->next;
+            }
+            
+        }
+    }
+};
+
+
+
+// { Driver Code Starts.
+int main()
+{
+    int t,n1;
+    cin>>t;
+    while (t--) {
+        cin>>n1;
+        int m,n;
+        cin>>m;
+        cin>>n;
+        insert(n1);
+        Solution ob;
+        ob.linkdelete(start,m,n);
+        printList(start);
+    }
+    
+    return 0;
+}
+  // } Driver Code Ends
