03437f9aee2e86f6f38435bca8569ba5cb84bcae
Vishakha <vvikharankar@students.vnit.ac.in>
2022-01-18 16:16:41 +0530

    Adobe-Que10

---
+++ 56-leaders-in-an-array.cpp
@@ -0,0 +1,65 @@
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n)
+    {
+        vector<int> sol;
+        n--;
+        sol.push_back(a[n]);
+        int max = a[n];
+        n--;
+        while(n >= 0)
+        {
+            if(a[n] >= max)
+            {
+                max = a[n];
+                sol.push_back(max);
+            }
+            n--;
+        }
+        reverse(sol.begin(), sol.end());
+        return sol;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+  // } Driver Code Ends
